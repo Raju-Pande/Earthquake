8b48d7a6677de44e1331759b9af8a59854199c24
raj <rpande400@gmail.com>
2023-07-26 09:08:28 +0530

    earth

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -20,7 +20,7 @@ export const Map = () => {
     L.Marker.prototype.options.icon = DefaultIcon;
 
     useEffect(() => {
-        axios.get("http://localhost:3001/api/data")
+        axios.get("https://earths.onrender.com/api/data")
           .then(response => {
             setEarthquakes(response.data);
           })
